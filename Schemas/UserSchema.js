108837a8cb94bad408737e304b9dc5603f1c0221
gabeh33 <gabeholmes1@gmail.com>
2024-11-11 13:04:50 -0500

    Replaced bcrypt with bcryptjs

--- Schemas/UserSchema.js
+++ Schemas/UserSchema.js
@@ -1,6 +1,8 @@
 // models/User.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcryptjs');
+
+
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
