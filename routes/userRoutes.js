0c45c5a41ff8f142afe6a113e7a3e09664dadbfd
gabeh33 <gabeholmes1@gmail.com>
2024-11-20 23:19:58 -0500

    bug fixes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const { getProfile, depositMoney, upload } = require('../controllers/userController');
-
+const authenticateToken = require('../middleware/authenticateToken');
 
 // Profile and deposit routes
 router.get('/', getProfile);
 router.post('/deposit', depositMoney);
-router.post('/upload', upload);
+router.post('/upload', authenticateToken, upload);
 
 module.exports = router;
